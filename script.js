0c0347ffa16ed832d0cbdc404d9fb61f1ea135df
Elle <93220634+codes-elle@users.noreply.github.com>
2022-07-28 12:31:31 -0400

    Added the ids with checkboxes

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById('uppercase') = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+document.getElementById('lowercase') = "abcdefghijklmnopqrstuvwxyz";
+document.getElementById('numeric') = "0123456789";
+document.getElementById('symbols') = "!@#$%^&-";
