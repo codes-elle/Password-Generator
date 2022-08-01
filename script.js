654adabd1223308573947c6529cf5fda126153e3
Elle <93220634+codes-elle@users.noreply.github.com>
2022-08-01 09:33:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,17 @@
 
 
 
+const outputEl = document.getElementById('output');
+const copyToClipEl = document.getElementById('copyToClipboard');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numericEl = document.getElementById('numeric');
+const symbolsEl = document.getElementById('symbols');
+const generatePassword = document.getElementById('generateButton');
 
 
-document.getElementById('uppercase') = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-document.getElementById('lowercase') = "abcdefghijklmnopqrstuvwxyz";
-document.getElementById('numeric') = "0123456789";
-document.getElementById('symbols') = "!@#$%^&-";
+// uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// lowercase = "abcdefghijklmnopqrstuvwxyz"
+//numeric = "0123456789"
+//symbols = "!@#$%^&-"
